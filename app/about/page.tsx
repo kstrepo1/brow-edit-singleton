1eb0e1030d5bfb8565bc6898d6ac43c327e716ee
kstrepo1 <kail.terepai@griffithuni.edu.au>
2026-08-05 05:46:21 +1000

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,15 +7,34 @@ export default function About() {
     <Header/>   
     
     <div className="pageContent">
-        Tegan Sinclair
-        Brow and Lash Artist
+
+      <div className="flex">
+        <div className="aboutBlock flex-column flex-1 m-10">
+          
+          <Image src="/tegan.jpg" height={200} width={200} alt="Image of the founder Tegan" className="rounded-full"/>
+          
+          <div className="mt-3">
+            Tegan Sinclair
+          </div>
+
+          <div className="text-gray-400">
+            Brow and Lash Artist
+          </div>
+          
+        </div>
+        <div className="flex-3 flex items-center">
+          <div>
         Hi, I'm Tegan!
 
         Since beginning my career in 2017, I've specialised in creating soft, natural brows and lash lifts tailored to suit your unique features. With experience in both New Zealand and Australia, I've had the opportunity to learn from some of the best artists in the industry.
 
         I'm now based in Singleton, NSW, and can't wait to welcome you and help you feel your most confident.
-    
+       
+          </div>
+          </div>
+      
+      </div>
     </div>
-    </div>
+  </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,23 @@ export default function Home() {
       <Header/>
 
       <div className="pageContent">
-        The Brow Edit specialises in custom brows and lash lifts, completely tailored to you.
+        <Image
+        src="/banner.png"
+        height={300}
+        width={800}
+        alt="A 3 split banner showing the practice chair in a warm and inviting environment, a fresh brow and lash treatment, and a photo of the founder, Tegan"
+        
+        />
+        <div className="textBlock">
+          <h2>The Brow Edit specialises in custom brows and lash lifts, completely tailored to you.</h2>
+          
 
-        Founded by Tegan, we are an inclusive space where everyone is welcome. Our goal is to make you feel confident, comfortable, and beautiful every time you visit.
+          Founded by Tegan, we are an inclusive space where everyone is welcome. Our goal is to make you feel confident, comfortable, and beautiful every time you visit.
 
-        With 8 years of experience across New Zealand and Australia, we're excited to now be in Singleton, located inside the beautiful Haus of Artistry.
+          With 8 years of experience across New Zealand and Australia, we're excited to now be in Singleton, located inside the beautiful Haus of Artistry.
 
-        We can't wait to meet you soon!
+          We can't wait to meet you soon!
+        </div>
       </div>
     </div>
 
